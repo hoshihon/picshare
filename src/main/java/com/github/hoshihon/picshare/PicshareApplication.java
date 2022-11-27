2a5660cd317f4ca8f91e16994d04d7011b50d465
hoshihon <hoshihon11@gmail.com>
2022-11-27 18:05:18 +0800

    初始化 maven 工程

---
+++ src/main/java/com/github/hoshihon/picshare/PicshareApplication.java
@@ -0,0 +1,13 @@
+package com.github.hoshihon.picshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicshareApplication.class, args);
+    }
+
+}
